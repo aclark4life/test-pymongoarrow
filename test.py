6250d337cf877b072b39d40a5f458da951d7ac97
Jeffrey A. Clark <aclark@aclark.net>
2024-09-27 08:02:58 -0400

    Update project-makefile files

--- test.py
+++ test.py
@@ -6,11 +6,11 @@ import os
 uri = os.environ.get("DATABASE_URL")
 
 # Create a new client and connect to the server
-client = MongoClient(uri, server_api=ServerApi('1'))
+client = MongoClient(uri, server_api=ServerApi("1"))
 
 # Send a ping to confirm a successful connection
 try:
-    client.admin.command('ping')
+    client.admin.command("ping")
     print("Pinged your deployment. You successfully connected to MongoDB!")
 
 except Exception as e:
